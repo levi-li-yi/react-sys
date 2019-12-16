a60bb9d7c1b54007680e23479eb98fc415b8f04c
liyi <liyi@sinux.com>
2019-12-16 14:34:45 +0800

    add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, {Suspense} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import router from './router/index';
 import './App.css';
-import {Provider} from 'react-redux';
+//import {Provider} from 'react-redux';
+import {StoreContext} from 'redux-react-hook';
+/*无论使用react-redux还是redux-react-hook;redux部分均在store中不变*/
 import store from './store'
 
 function App() {
@@ -10,8 +12,8 @@ function App() {
     return (
     <Router>
         <Suspense fallback={<div>Loading...</div>}>
-            {/*将store统一当做props注入全局*/}
-            <Provider store={store}>
+            {/*使用react-redux将store统一当做props注入全局*/}
+            {/*<Provider store={store}>
                 <div className="App">
                     {
                         router.map(({path, Components, exact = true, routes = []}, key) => {
@@ -25,7 +27,23 @@ function App() {
                         })
                     }
                 </div>
-            </Provider>
+            </Provider>*/}
+            {/*使用redux-react-hook将store统一当做props注入全局*/}
+            <StoreContext.Provider value={store}>
+                <div className="App">
+                    {
+                        router.map(({path, Components, exact = true, routes = []}, key) => {
+                            return <Route
+                                path={path}
+                                exact={exact}
+                                key={key}
+                                render={props => (
+                                    <Components {...props} routes={routes}/>
+                                )}/>
+                        })
+                    }
+                </div>
+            </StoreContext.Provider>
         </Suspense>
     </Router>
   );
--- src/page/hooksPage/reduxPage/ReduxPage.js
+++ src/page/hooksPage/reduxPage/ReduxPage.js
@@ -1,8 +1,38 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {Input, Button} from 'antd'
+import {useMappedState, useDispatch} from 'redux-react-hook';
+import {types} from 'store/dictStore'
 
-function ReduxPage() {
+/*在react hooks中使用redux-react-hook做的状态管理*/
+function ReduxPage(props) {
+    const inputTypeValue = useMappedState(state => state.dictReducer.inputTypeValue);
+    const typeList = useMappedState(state => state.dictReducer.typeList);
+    const dispatch = useDispatch();
+    function inputChange(e) {
+        const action = {
+            type: types.CHANGE,
+            value: e.target.value
+        };
+        dispatch(action)
+    }
+    function addItem() {
+        const action  = {
+            type: types.ADD
+        };
+        dispatch(action)
+    }
     return (
-        <div>ReduxPage</div>
+        <div>
+            <Input placeholder="请输入" value={inputTypeValue} onChange={inputChange}/>
+            <Button onClick={addItem}>提交</Button>
+            <ul>
+                {
+                    typeList.map((item, index) => {
+                        return <li key={index}>{item}</li>
+                    })
+                }
+            </ul>
+        </div>
     )
 }
 
